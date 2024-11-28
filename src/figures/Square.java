d60b5bec995d8700484eb47af1e814126fc20616
eparedesr <eparedesr1@est.ups.edu.ec>
2024-11-28 13:08:04 -0500

    Add(I): Add Square Drawing and Calculations methods

--- src/figures/Square.java
+++ src/figures/Square.java
@@ -5,23 +5,37 @@ public class Square {
 
     public Square(){}
 
-    public Square(int length){this.length=length;}
+    public Square(int length){this.length = length;}
 
     //method draw square
     public void drawSquare(){
-        if(length <= 1){
-            System.out.println("Line length ");
+        if(length < 1){
+            System.out.println("There is no length");
         } else {
-            System.out.println("Drawing Line with length: " + length + "[u]" + "\n");
+            System.out.println("=================================");
+            System.out.println("Drawing Square with length: " + length + "[u]" + "\n");
             for(int i = 0; i< length; i++){
                 for (int j = 0; j< length; j++){
                     System.out.print(" * ");
                 }
                 System.out.println();
             }
-            System.out.println("\n");
-            System.out.println("----------------");
+            System.out.println("=================================");
+            System.out.println("Perimeter: " + length + " * 4 = " + calculatePerimeter());
+            System.out.println("=================================");
+            System.out.println("Area: " + length + " * " + length + " = " + calculateArea());
+            System.out.println("=================================");
         }
+    }
+
+    //method obtains Perimeter
+    public int calculatePerimeter(){
+        return length *4;
+    }
+
+    //method obtains Area
+    public int calculateArea(){
+        return length * length;
     }
 
     //getter and setter
